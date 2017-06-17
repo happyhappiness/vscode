28c5961cc5c7ec747ba3fa891bdb051eacc10cd6
happyhappiness <993273596@qq.com>
2017-06-17 12:51:59 +0800

    reorgnize

---
+++ src-old/analyze_control_old_new.py
@@ -0,0 +1,340 @@
+#-*-coding: utf-8 -*-
+import csv
+import sys
+import re
+import commands
+import base64
+import json
+import myUtil
+import my_constant
+from itertools import islice
+from joern.all import JoernSteps
+
+"""
+@ param  result list
+@ return list of analyzed node info
+@ callee ...
+@ caller getDependendedAstOfLog ..
+@ analyze query node. include : id, isCFGNode, type, code
+"""
+def nodeAnalysis(result):
+
+    cfg_list = []
+    for record in result:
+        value_list = []
+        value_list.append(str(record._id))
+        value_list.append(record.properties['isCFGNode'])
+        value_list.append(record.properties['type'])
+        value_list.append(record.properties['code'])
+        cfg_list.append(value_list)
+
+    return cfg_list
+
+"""
+@ param  ddg_node[ddg, variable], ddg_lists and joern instance of the python-joern api
+@ return ddg_lists
+@ callee ...
+@ caller getDependendedAstOfLog ..
+@ involve get ddg list by changing variables into type and callee into function name ...  
+"""
+def getDDGList(ddg_node, ddg_lists, joern_instance):
+    # get type
+    if not ddg_node:
+        return ddg_lists
+
+    # get statement and variable
+    ddg = ddg_node[1]
+    ddg_id = str(ddg[0])
+    variable = ddg_node[0]
+
+    # get use variable map(variable name, variable type)
+    var_query = '_().getVarMap(' + ddg_id +')'
+    ddg_var_map = joern_instance.runGremlinQuery(var_query)[0]
+    # make dictionary based on query result
+    var_dict = {}
+    for var_map in ddg_var_map:
+        var_dict[var_map[0]] = var_map[1]
+    # get def variable map(variable name, variable type)
+    var_query = '_().getVarMapForDDG(' + ddg_id +')'
+    ddg_var_map = joern_instance.runGremlinQuery(var_query)[0]
+    # append dictionary
+    for var_map in ddg_var_map:
+        var_dict[var_map[0]] = var_map[1]
+
+
+    # get statement list(postfix)
+    ddg_query = '_().getCFGStatementLists(' + ddg_id + ')'
+    ddg_list = joern_instance.runGremlinQuery(ddg_query)[0]
+
+    # update ddg list to typed ddg list
+    for var in ddg_list:
+        if var is None:
+            continue
+        # update var to type if find in dictionary
+        if var_dict.has_key(var):
+            ddg_list[ddg_list.index(var)] = var_dict[var]
+            continue
+        # remove namespace before::
+        var = myUtil.removeNamespace(var)
+
+    # update variable to variable type
+    variable = var_dict.get(variable, variable)
+    # add type ddg into ddg lists of log statement
+    # context_lists.append([cond, control_label])
+    ddg_lists.append([ddg_list, variable])
+
+    return ddg_lists
+
+"""
+@ param  neighbor,  context_lists and joern instance of the python-joern api
+@ return context_lists
+@ callee ...
+@ caller getDependendedAstOfLog ..
+@ involve get neighbor list by changing variables into type and callee into function name ...  
+"""
+def getNeighborList(neighbor, context_lists, joern_instance):
+
+    # get type
+    if not neighbor:
+        return context_lists
+
+    control_label = ""
+
+    # get neighbor node
+    neighbor_node = neighbor[1]
+    neighbor_id = str(neighbor_node[0])
+
+
+    # get use variable map(variable name, variable type)
+    var_query = '_().getVarMap(' + neighbor_id +')'
+    neighbor_var_map = joern_instance.runGremlinQuery(var_query)[0]
+    # make dictionary based on query result
+    var_dict = {}
+    for var_map in neighbor_var_map:
+        var_dict[var_map[0]] = var_map[1]
+    # get def variable map(variable name, variable type)
+    var_query = '_().getVarMapForDDG(' + neighbor_id +')'
+    neighbor_var_map = joern_instance.runGremlinQuery(var_query)[0]
+    # append dictionary
+    for var_map in neighbor_var_map:
+        var_dict[var_map[0]] = var_map[1]
+
+    # get neighbor statement(postfix)
+    neighbor_query = '_().getCFGStatementLists(' + neighbor_id + ')'
+    neighbor_list = joern_instance.runGremlinQuery(neighbor_query)[0]
+
+    # update neighbor list to typed neighbor list
+    for var in neighbor_list:
+        if var is None:
+            continue
+        # update var to type if find in dictionary
+        if var_dict.has_key(var):
+            neighbor_list[neighbor_list.index(var)] = var_dict[var]
+            continue
+        # remove namespace before::
+        var = myUtil.removeNamespace(var)
+
+
+    # add type neighbor into context lists of log statement
+    # context_lists.append([cond, control_label])
+    context_lists.append([neighbor_list])
+
+    return context_lists # cond + control_label
+
+
+"""
+@ param  condition, context_lists and joern instance of the python-joern api
+@ return context_lists
+@ callee ...
+@ caller getDependendedAstOfLog ..
+@ involve get condition list by changing variables into type and callee into function name ...  
+"""
+def getCondList(condition, context_lists, joern_instance):
+
+    # get type
+    if not condition:
+        return context_lists
+
+    if not condition[0]:
+        control_label = ''
+    else:
+        control_label = condition[0]
+
+    # get condition node
+    cond_node = condition[1]
+    cond_id = str(cond_node[0])
+
+
+    # get condition variable map(variable name, variable type)
+    var_query = '_().getVarMap(' + cond_id +')'
+    cond_var_map = joern_instance.runGremlinQuery(var_query)[0]
+    # make dictionary based on query result
+    var_dict = {}
+    for var_map in cond_var_map:
+        var_dict[var_map[0]] = var_map[1]
+
+    cond_query = '_().getConditionListByid(' + cond_id + ')'
+    cond_list = joern_instance.runGremlinQuery(cond_query)[0] # just one cond
+    # get cond_list of this condition
+    is_case = control_label.startswith("case")
+    if is_case:
+        # switch case
+        cond_list[0].append(control_label[4:].strip())
+        cond_list[0].append('==')
+
+    # update condition list to typed condition list
+    for cond in cond_list:
+        for var in cond:
+            if var is None:
+                continue
+            # update var to type if find in dictionary
+            if var_dict.has_key(var):
+                cond[cond.index(var)] = var_dict[var]
+                continue
+            # remove namespace before::
+            var = myUtil.removeNamespace(var)
+
+        # add type condition into conditin lists of log statement
+        # context_lists.append([cond, control_label])
+        context_lists.append([cond])
+
+    return context_lists # cond + control_label
+
+"""
+@ param  is delete log or not, old and new location and filename in file, joern instance of the python-joern api
+@ return log, cdg_list, neighbor_list, context_lists, ddg_node, ddg_lists
+@ callee cdgNodeAnalysis
+@ caller main ..
+@ involve retrieve ast of dependency node, include data dependence and control dependence
+"""
+def getDependendedAstOfLog(change_type, old_loc, old_fileName, new_loc, new_fileName, joern_instance):
+
+    # initialize log, cfg_list, ddg_list
+    old_loc = old_loc + ":"
+    new_loc = new_loc + ":"
+    # query for log statement
+    base_query = '_().getLogByFileAndLoc("' + old_fileName + '","' + old_loc + '")'
+    old_log_result = joern_instance.runGremlinQuery(base_query)[0]
+    base_query = '_().getLogByFileAndLoc("' + new_fileName + '","' + new_loc + '")'
+    new_log_result = joern_instance.runGremlinQuery(base_query)[0]
+
+    old_context_list = []
+    new_context_list = []
+    old_ddg_list = []
+    old_static_list = []
+    new_ddg_list = []
+    new_static_list = []
+    old_log = []
+    new_log = []
+    old_log_id = -1
+    new_log_id = -1
+    # context length
+    contxt_len = 3
+    order = str(contxt_len)
+
+    # get old context_list
+    if old_log_result:
+        old_log = nodeAnalysis(old_log_result)[0]
+        old_log_id = old_log[0]
+        old_context_list = myUtil.getContext(old_log_id, joern_instance)
+        old_ddg_list, old_static_list = myUtil.getDDGAndContent(old_log_id, joern_instance)
+
+     # get new context_list
+    if new_log_result:
+        new_log = nodeAnalysis(new_log_result)[0]
+        new_log_id = new_log[0]
+        new_context_list = myUtil.getContext(new_log_id, joern_instance)
+        new_ddg_list, new_static_list = myUtil.getDDGAndContent(new_log_id, joern_instance)
+
+    # # get ddg list and static list (-: old, + new)
+    if change_type == my_constant.LOG_ADD:
+        log = new_log
+    else:
+        log = old_log
+    # if log_id != -1:
+    #     ddg_list, static_list = myUtil.getDDGAndContent(log_id, joern_instance)
+
+    return log, old_context_list, new_context_list, old_ddg_list, old_static_list, new_ddg_list, new_static_list
+
+"""
+@ param  record of fetch_reader line, change_type, 
+@ return edited record array, has edited flag(log)
+@ caller analyze_old_new
+@ callee getDependendedAstOfLog
+@ involve retrieve old and new contexts of log statements and store it
+"""
+def analyze_record( joern_instance, record, change_type):
+
+    old_loc = record[my_constant.FETCH_LOG_OLD_LOC]
+    new_loc = record[my_constant.FETCH_LOG_NEW_LOC]
+    old_fileName = record[my_constant.FETCH_LOG_OLD_FILE]
+    new_fileName = record[my_constant.FETCH_LOG_NEW_FILE]
+
+    # query database with loc and filename
+    log, old_context_list, new_context_list, \
+        old_ddg_list, old_static_list, new_ddg_list, new_static_list \
+= getDependendedAstOfLog(change_type, old_loc, old_fileName, new_loc, new_fileName, joern_instance)
+
+    # record the info: log node
+    record[my_constant.FETCH_LOG_LOG] = json.dumps(log)
+    # cdg ndoe, neighbor node, context_lists
+    record.append(json.dumps(old_context_list))
+    record.append(json.dumps(new_context_list))
+    # ddg node, ddg lists
+    record.append(json.dumps(old_ddg_list))
+    record.append(json.dumps(old_static_list))
+    record.append(json.dumps(new_ddg_list))
+    record.append(json.dumps(new_static_list))
+
+    return record, log
+
+"""
+@ param ...
+@ return nothing
+@ caller main
+@ callee analyze_record
+@ involve operate fetch and analyze files
+"""
+def analyze_old_new():
+
+    fetch = file(my_constant.FETCH_LOG_FILE_NAME, 'rb')
+    # initialize write file
+    analysis = file(my_constant.ANALYZE_OLD_NEW_FILE_NAME, 'wb')
+    analyze_writer = csv.writer(analysis)
+    analyze_writer.writerow(my_constant.ANALYZE_OLD_NEW_TITLE)
+    # initialize read file
+    records = csv.reader(fetch)
+    # initialize python-joern instance
+    joern_instance = JoernSteps()
+    joern_instance.addStepsDir("/data/joern-code/query/")
+    joern_instance.setGraphDbURL("http://localhost:7474/db/data/")
+    # connect to database
+    joern_instance.connectToDatabase()
+
+    # traverse the fetch csv file to update context info
+    count = 0
+    # remove the table title
+    for record in islice(records, 1, None):
+        if count % 10 == 0:
+            print "now record the No. %d analyze" %count
+        # call deal_line to retrieve location info(- included)
+        record, log = analyze_record(joern_instance, record, record[my_constant.FETCH_LOG_CHANGE_TYPE])
+
+        # update analyze data is has edited
+        if log:
+            # record.pop(0)
+            # record.pop(6)
+            analyze_writer.writerow(record)
+            count = count + 1
+
+    # close files
+    analysis.close()
+    fetch.close()
+
+
+"""
+main function
+"""
+if __name__ == "__main__":
+
+    analyze_old_new()
---
+++ src-old/myUtil.py
@@ -0,0 +1,279 @@
+#-*-coding: utf-8 -*-
+import similarity_func
+import test_cluster_control
+import analyze_control_old_new
+import my_constant
+import csv
+from itertools import islice
+
+"""
+@ param  var to deal with
+@ return list of analyzed node info
+@ callee ...
+@ caller analyze_control.py, similarity_func.py ..
+@ involve remove namespace bafore ::,
+          and remove caller before ->, .
+          and remove parameter after (
+"""
+def removeNamespace(name):
+    # remove namespace before ::
+    startpos = name.find("::")
+    while startpos != -1:
+        name = name[startpos + 2:]
+        startpos = name.find("::")
+
+    # remove caller before ->
+    startpos = name.find("->")
+    while startpos != -1:
+        name = name[startpos + 2:]
+        startpos = name.find("->")
+
+    # remove caller before .
+    startpos = name.find(".")
+    while startpos != -1:
+        name = name[startpos + 2:]
+        startpos = name.find(".")
+
+    # # remove param after (
+    # startpos = name.find("(")
+    # while startpos != -1:
+    #     name = name[:startpos]
+    #     startpos = name.find("(")
+    # name = name.strip()
+
+    return name
+
+
+"""
+@ param  element and in_list
+@ return in_list
+@ callee ...
+@ caller cluster_control
+@ involve remove given element in in_list
+"""
+def removeGivenElement(element, in_list):
+    # remove element in in_list
+    while element in in_list:
+        in_list.remove(element)
+
+    return in_list
+
+"""
+@ param  isFromRead
+@ return function_similarity_dic
+@ callee similarity_func.getFunctionSimilarity
+@ caller cluster_context_with_function
+@ involve call similarity_func.getFunctionSimilarity to get similarity_dic between functions
+"""
+def getFunctionSimilarityDic(isFromRead):
+
+    func_similarity_dic = {}
+    if isFromRead:
+        # initialize func_similarity_dict
+        analyze_func = file(my_constant.FUNC_SIMILAIRTY_FILE_NAME, 'rb')
+        func_records = csv.reader(analyze_func)
+        for func_record in islice(func_records, 1, None):
+            func_similarity_dic[(func_record[0], func_record[1])] = func_record[2]
+        analyze_func.close()
+    else:
+        func_similarity_dic = similarity_func.getFunctionSimilarity()
+    return func_similarity_dic
+
+"""
+@ param  similarity_dic, filename
+@ return ...
+@ callee ...
+@ caller self, cluster_context_with_function
+@ involve dump similarity_dic
+"""
+def dumpSimilarityDic(similarity_dic):
+
+    similarity_control = file(my_constant.REPOS_SIMILARITY_FILE_NAME, 'wb')
+    similarity_control_writer = csv.writer(similarity_control)
+    similarity_control_writer.writerow(["left", "right", "similarity"])
+    # dump similarity
+    for pair, similarity in similarity_dic.items():
+        if pair[0] >= 0 and pair[1] >= 0:
+            similarity_control_writer.writerow([pair[0], pair[1], similarity])
+
+    # close files
+    similarity_control.close()
+
+
+"""
+@ param  dict, key_index
+@ return dict
+@ callee ...
+@ caller cluster_control.computeSim
+@ involve remove key contains key_a or key_b specified key_index
+"""
+def removeDicElement(dictionary, key_index):
+
+    key_list = dictionary.keys()
+    key_a = key_list[key_index][0]
+    key_b = key_list[key_index][1]
+    for key in key_list:
+        # if key contains key_a or key_b
+        if key[0] == key_a or key[1] == key_b:
+            del dictionary[key]
+
+    return dictionary
+
+
+"""
+@ param ...
+@ return log_functions
+@ caller cluster_context_with_function
+@ callee ...
+@ involve retrieve log function name from given file
+"""
+def retrieveLogFunction(fileName):
+    log_functions = []
+    log_statement = open(fileName, 'rb')
+    lines = log_statement.readlines()
+
+    for line in lines:
+        log_function = line[0:line.find("@")]
+        if not log_function in log_functions:
+            log_functions.append(log_function)
+
+    log_statement.close()
+    return log_functions
+
+"""
+@ param log functions
+@ return regrex_string e.g. 'assert|log|debug|print|write|error'
+@ caller fetch
+@ callee ...
+@ involve concate log functions into regrex string
+"""
+def functionToRegrexStr(log_functions):
+    regrex_string = ''
+    for log_function in log_functions:
+        regrex_string += log_function + '|'
+
+    return regrex_string[:-1]
+
+
+"""
+@ param log id and joernInstance
+@ return context list
+@ caller analyze_control_repos
+@ callee analyze_control_old_new, analyze_control_old_new
+@ involve get context info with given log id and joern_instance
+"""
+def getContext(log_id, joern_instance):
+    context_list = []
+    order = str(3)
+    # condition node list (control type, id, isCFGNode, type, code)
+    cdg_query = '_().getCFGControlByLog(' + log_id + ',' + order +')'
+    cdg_list = joern_instance.runGremlinQuery(cdg_query)[0] # true/false + cond_node
+    # deal with each condition
+    for condition in cdg_list:
+        context_list = analyze_control_old_new.getCondList(condition, context_list, joern_instance)
+
+    # add flowlabel to make sure the total context count is at least 5
+    # context = control + neighbor
+    # if len(cdg_list) < 1:
+    if len(cdg_list) < 1:
+        neighbor_query = '_().getCFGStatementByLog(' + log_id + ',' + order +')'
+        neighbor_list = joern_instance.runGremlinQuery(neighbor_query)[0] #  + cond_node
+        # deal with each neighbor
+        for neighbor in neighbor_list:
+            context_list = analyze_control_old_new.getNeighborList(neighbor, context_list, joern_instance)
+
+    return context_list
+
+"""
+@ param log id and joernInstance
+@ return ddg list and static list
+@ caller analyze_control_repos, analyze_control_old_new
+@ callee analyze_control_old_new
+@ involve get context info with given log id and joern_instance
+"""
+def getDDGAndContent(log_id, joern_instance):
+    ddg_list = []
+    static_list = []
+    ddg_query = '_().getDDG(' + log_id + ')'
+    ddg_node = joern_instance.runGremlinQuery(ddg_query)[0]
+    # deal with each ddg_node to get ddg_lists
+    for ddg_node in ddg_node:
+        ddg_list = analyze_control_old_new.getDDGList(ddg_node, ddg_list, joern_instance)
+
+    # get static string
+    static_query = '_().getStaticStr(' + log_id + ')'
+    static_list = joern_instance.runGremlinQuery(static_query)[0]
+
+    return ddg_list, static_list
+
+"""
+@ param  cond_list of a and b to compute, func_similarity_dic
+@ return lenth of common substring / min length
+@ callee ...
+@ caller compute_context_similarity ..
+@ involve compute the longgest common string (continuous) of two cond_list
+"""
+def longestCommon(list_a, list_b):
+
+    # if just one element, then compare that
+    len_a = len(list_a)
+    len_b = len(list_b)
+    if len_a == 0 or len_b == 0:
+        if (len_a + len_b) == 0:
+            return float(1)
+        else:
+            return float(0)
+    # length of a and b
+    len_a += 1
+    len_b += 1
+
+    # find first common and build the common matrix(a*b)
+    memory = [[0 for col in range(len_b)] for row in range(len_a)]
+    index_a = 0
+    index_b = 0
+    first_len_common = 0
+    for i in range(1, len_a):
+        for j in range(1, len_b):
+            # update len_common with history
+            if list_a[i - 1] == list_b[j-1]:
+                memory[i][j] = memory[i-1][j-1] + 1
+                if first_len_common < memory[i][j]:
+                    first_len_common = memory[i][j]
+                    index_a = i
+                    index_b = j
+                continue
+            else:
+                memory[i][j] = 0
+
+    len_common = first_len_common
+    # filter the totally different and equal condition
+    if len_common in range(1, min(len_a, len_b)):
+        curr_a = 0
+        curr_b = 0
+        # find all common in road decide by index_a, index_b
+        # forward [1, index_a - 1]
+        for i in range(1, min(index_a, index_b)):
+            # last common <-- element behind is 0
+            curr_a = index_a - i
+            curr_b = index_b - i
+            if memory[curr_a][curr_b] > 0 and memory[curr_a + 1][curr_b + 1] == 0:
+                len_common += memory[curr_a][curr_b]
+
+        # backward [index_a + 1, len_a - 1]
+        for i in range(1, min(len_a - index_a, len_b - index_b)):
+            # last common <-- element behind is 0
+            curr_a = index_a + i
+            curr_b = index_b + i
+            if memory[curr_a][curr_b] > 0 and \
+                (curr_a == len_a - 1 or curr_b == len_b - 1 or memory[curr_a + 1][curr_b + 1] == 0):
+                len_common += memory[curr_a][curr_b]
+
+    # simlarity value with common length / min length (0, 1)
+    # return float(len_common)/min(len_a, len_b)
+    return  len_common
+
+
+
+# list_a = [[["chroot", "char [ MAX_STRING_LENGTH + 1 ]"]], [["!", "strcasecmp", "config_getoption", "\"DO_CHROOT\"", "\"yes\""]], [["!", "char *", "0"]], [["int"]], [["!", "char *", "0"]], [["!", "int"]]]
+# list_b = [[["chroot", "char *"]], [["!", "strcasecmp", "config_getoption", "\"DO_CHROOT\"", "\"yes\""]], [["!", "char *", "0"]], [["int"]], [["!", "char *", "0"]], [["!", "int"]]]
+# cluster_control.computeSim(list_a, list_b, {})
