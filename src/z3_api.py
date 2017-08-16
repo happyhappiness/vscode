bfe19fd8f6e93bccd80aa86a5cba6e731366736e
happyhappiness <993273596@qq.com>
2017-08-16 10:48:06 +0800

    new bftpd data with null and macro

--- src/z3_api.py
+++ src/z3_api.py
@@ -1,12 +1,20 @@
 from z3 import *
 
 if __name__ == "__main__":
-    x = Int('x')
-    y = Int('y')
+    
     s = Solver()
-    s.add(x + y > 5, Not(x + y > 5))
+    s.push()
+    int_a, int_b = Bools('int int')
+    func_a, func_b = Bools('func func')
+    null_a, null_b = Bools('null null')
+    stmt_one = Or(And(int_a, func_a), null_a)
+    stmt_two = And(Or(int_b, null_b), Or(func_b, null_b))
+    # decide whether two statement is equal
+    s.add( Xor(stmt_one, stmt_two) )
     status = s.check()
     print status
-    if status == 'sat':
+    if status.r == 1:
         print s.model()
-
+    else:
+        print "stmt one <=> stmt two"
+    s.pop()
