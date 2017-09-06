2c5ffd6349b4b916ed5323733d1e45722d03a40a
happyhappiness <993273596@qq.com>
2017-09-06 11:13:14 +0800

    new cluster result

--- src/analyze_control_old_new.py
+++ src/analyze_control_old_new.py
@@ -152,20 +152,26 @@ def analyze_old_new_joern(is_rebuild = False):
         if joern.set_log(old_function_file, int(old_loc)):
             ddg = json.dumps(joern.get_argument_type())
             cdg = json.dumps(joern.get_normalized_control_dependence())
-            if joern.set_log(new_function_file, int(new_loc)):
-                ddg_codes, ddg_locs = joern.get_data_dependence_for_cdg_and_log()
-                # check if ddg is edited with gumtree
-                gumtree.set_old_new_file(new_function_file, old_function_file)
-                is_ddg_modified = gumtree.get_function_edited_type(ddg_locs)
-                if is_ddg_modified:
-                    record[my_constant.FETCH_LOG_ACTION_TYPE] = my_constant.LOG_LOG_FEATURE_MODIFY
-                else:
-                    record[my_constant.FETCH_LOG_ACTION_TYPE] = my_constant.LOG_LOG_MODIFY
-            # if miss new log then keep original type
-            # else:
-            #     record[my_constant.FETCH_LOG_ACTION_TYPE] = my_constant.LOG_LOG_MODIFY
-            old_new_joern_writer.writerow(record + [ddg, cdg, ddg_codes, ddg_locs])
-            total_log += 1
+        # miss old log(inserted log)
+        else:
+            ddg = json.dumps([])
+            cdg = json.dumps([])
+        if joern.set_log(new_function_file, int(new_loc)):
+            ddg_codes, ddg_locs = joern.get_data_dependence_for_cdg_and_log()
+            # check if ddg is edited with gumtree
+            gumtree.set_old_new_file(new_function_file, old_function_file)
+            is_ddg_modified = gumtree.get_function_edited_type(ddg_locs)
+            if is_ddg_modified:
+                record[my_constant.FETCH_LOG_ACTION_TYPE] = my_constant.LOG_LOG_FEATURE_MODIFY
+            else:
+                record[my_constant.FETCH_LOG_ACTION_TYPE] = my_constant.LOG_LOG_MODIFY
+        # if miss new log(deleted log) then keep original type
+        else:
+            # record[my_constant.FETCH_LOG_ACTION_TYPE] = my_constant.LOG_LOG_MODIFY
+            ddg_codes = set()
+            ddg_locs = set()
+        old_new_joern_writer.writerow(record + [ddg, cdg, ddg_codes, ddg_locs])
+        total_log += 1
         print 'have dealed with %d record; %d log' %(total_record, total_log)
         total_record += 1
 
