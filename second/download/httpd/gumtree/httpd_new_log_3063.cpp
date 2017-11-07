bcbae6497e976f1c8556d603200e1b81a6da288a
happyhappiness <993273596@qq.com>
2017-11-07 16:02:41 +0800

    mutt analysis result

--- second/download/httpd/gumtree/httpd_new_log_3063.cpp
+++ second/download/httpd/gumtree/httpd_new_log_3063.cpp
@@ -1,2 +1,2 @@
-ap_log_error(APLOG_MARK, APLOG_CRIT, rv, ctx->s, APLOGNO(02073)
-                     "Failed to accept localhost mulitcast on socket.");
+ap_log_error(APLOG_MARK, APLOG_CRIT, rv, ctx->s, APLOGNO(02072)
+                     "Failed to join multicast group");
--- second/download/httpd/gumtree/httpd_new_log_855.cpp
+++ second/download/httpd/gumtree/httpd_new_log_855.cpp
@@ -1,2 +1,3 @@
 ap_log_error(APLOG_MARK, APLOG_STARTUP, 0, NULL,
-                 "  -f file            : specify an alternate ServerConfigFile");
+                 "  -d directory       : specify an alternate initial "
+                 "ServerRoot");
